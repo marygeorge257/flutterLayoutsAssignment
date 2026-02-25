b1c1c63de0629e1e44f7abd3812a9b0c4784214e
marygeorge257 <marygeorge25785@gmail.com>
2026-02-25 14:24:00 +0200

    Add files via upload

--- HomePage.dart
+++ HomePage.dart
@@ -5,71 +5,48 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-         appBar: AppBar(
-           centerTitle: true,
-           title: Text("My Flutter App",style: TextStyle(fontSize: 35,fontWeight: FontWeight.bold),),
-           leading: Icon(Icons.ac_unit_sharp,size: 35,color: Colors.blue,),
-           actions: [
-             Padding(padding: EdgeInsets.only(right: 20), 
-                 child:Icon(Icons.add_alert_sharp,color: Colors.black,size: 35,)
-             ),
-           
-           ],
-         ),
-      body:
-      Padding(padding: EdgeInsets.all(20),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          SizedBox(width: 5,),
-          Container(
-            margin: EdgeInsets.all(20),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.all(Radius.circular(15)),
-              color: Colors.lightBlue,
-            ),
-            width: 350,
-            height: 250,
-            alignment: Alignment.topCenter,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                CircleAvatar(
-                  radius: 55,
-                  backgroundColor: Colors.grey,
-                  child: Icon(Icons.person,size: 35,fontWeight: FontWeight.bold,),
-                ),
-                SizedBox(height: 10,),
-                Text("Mary George",
-                style: TextStyle(fontSize: 27,fontWeight: FontWeight.bold),),
-                SizedBox(height: 5,),
-                Text("Flutter developer",
-                style: TextStyle(fontSize:20 ),
-                ),
-                SizedBox(height: 5,),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Icon(Icons.facebook,color: Colors.blue[900],),
-                    SizedBox(width: 50,),
-                    Icon(Icons.facebook,color: Colors.blue[900],),
-                    SizedBox(width: 50,),
-                    Icon(Icons.facebook,color: Colors.blue[900],),
-                    SizedBox(width: 50,),
-                  ],
-                )
+      appBar: AppBar(
+        title: Text("Flutter Advanced Layout Project",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 26),),
 
-              ],
-            )
+      ),
+      body: Column(
+        children: [
+          Stack(
+            children: [
+              Container(
+                color: Colors.lightBlue[200],
+                height: 200,
+              ),
+              Positioned(
+                  top: 30,
+               child: Text("Welcome To Flutter!",style: TextStyle(color: Colors.white,fontSize: 29,fontWeight: FontWeight.bold))
+              )
+            ],
 
           ),
+          Expanded(child: ListView.builder(
+            padding: EdgeInsets.all(10),
+              itemCount: 15,
+              itemBuilder: (context,index){
+                return Container(
+                  margin: EdgeInsets.all(5),
+                   width: 100,
+                  height: 50,
+                 decoration: BoxDecoration(
+                   color: Colors.primaries[index],
+                   borderRadius: BorderRadius.circular(10)
+                 ),
+                  child: ListTile(
+                    leading: Icon(Icons.star,size: 25,color: Colors.white,),
+                    title: Text("Item ${index+1}",style: TextStyle(fontSize: 25,color: Colors.white),),
+                  ),
+                );
+              }
+          )
+          )
         ],
       ),
-      )
+      
     );
-
   }
 }
-
